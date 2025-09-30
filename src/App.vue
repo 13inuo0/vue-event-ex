3e4f530aa2ecf3cf929541e23fdf5797d1623eba
13inuo0 <13inuo0@github.com>
2025-09-30 11:02:52 +0900

    이벤트 연습 모음

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <Event />
+    <FormEx />
+  </div>
+</template>
+<script setup>
+import Event from "./components/Event.vue";
+import FormEx from "./components/Form-ex.vue";
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    gap: 20px;
+}
+.container div{
+    flex: 1;
+    background-color: antiquewhite;
+}
+</style>
